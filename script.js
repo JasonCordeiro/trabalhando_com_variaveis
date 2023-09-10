e6b4602c6602f6cf08f405d1450dba927e3b408c
Jason <jason.miguel022@gmail.com>
2023-09-10 19:45:01 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,20 @@
+//Exercício 1
+//Faça um programa que exiba um decimal, um inteiro e uma string utilizando o console.log().
+
+//Exercício 2
+//Declare uma variável, atribua um número, use o método typeof para mostrar na tela a palavra "number".
+
+//Exercício 3
+//Declare uma variável, atribua um valor booleano, use o método typeof para mostrar na tela a palavra "boolean".
+
+//Exercício 4
+//Declare uma variável, atribua uma string, use o método typeof para mostrar na tela a palavra "string".
+
+//Exercício 5
+//Faça um programa que exiba duas variáveis, uma com atribuição true e outra false
+
+//Exercício 6
+//Usando uma variável, e o console.log(), mostre no console o valor 'null'
+
+//Exercício 7
+//Usando uma variável, e o console.log(), mostre no console o valor 'undefined'
